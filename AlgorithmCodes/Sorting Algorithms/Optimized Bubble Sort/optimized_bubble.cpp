450979e3e9cf2def61566afe1a7d1abf2004d18c
Pervage-Ahmed-Tuhin <parvageahmedtuhin123456@gmail.com>
2024-03-28 22:01:35 +0600

    This is the main repo where i will add the algo codes

---
+++ AlgorithmCodes/Sorting Algorithms/Optimized Bubble Sort/optimized_bubble.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm> // include the algorithm header for std::swap
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swaps = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swaps = true;
+            }
+        }
+        if (!swaps) {
+            // If no swaps were made in an iteration, the array is already sorted
+            break;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Unsorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
